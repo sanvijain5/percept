1560f533c9502545aa9d71435d22e911f9a0f515
Sanvi Jain <sanvijain1703@gmail.com>
2026-08-17 00:04:20 +0530

    Configure production server for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -7,9 +8,11 @@ export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
+
   plugins: [
-    tanstackStart(),
-    viteReact(),
     tailwindcss(),
+    tanstackStart(),
+    nitro(),
+    viteReact(),
   ],
 });
